1bcd0c926e018fafc11fb956b9c8e96abd545ecf
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2020-06-02 07:16:50 +0100

    Update notebook post_save hook

--- tutorials/jupyter_notebook_config.py
+++ tutorials/jupyter_notebook_config.py
@@ -2,11 +2,15 @@
 from subprocess import check_call
 from pathlib import Path
 
+GIT = False
+
 def post_save(model, os_path, contents_manager):
     """post-save hook for converting notebooks to .py scripts"""
     if model['type'] != 'notebook':
         return # only do this for notebooks
     if os_path.endswith('.py'):
+        return
+    if 'Untitled' in os_path:
         return
     p = Path(os_path)
     for j in range(9):
@@ -20,7 +24,16 @@ def post_save(model, os_path, contents_manager):
         f.rename(p.parent / f'{p.stem}.py~{1}~')
     except:
         pass
-    check_call(['jupyter', 'nbconvert', '--to', 'script', p.name], cwd=str(p.parent))
+    try:
+        check_call(['jupyter', 'nbconvert', '--to', 'script', p.name], cwd=str(p.parent))
+        if GIT:
+            try:
+                check_call(['git', 'add', f'{p.stem}.py'], cwd=str(p.parent))
+                check_call(['git', 'commit', '-m', f'"Update {p.name}"'])
+            except:
+                pass
+    except:
+        pass
 
 c.FileContentsManager.post_save_hook = post_save
 
